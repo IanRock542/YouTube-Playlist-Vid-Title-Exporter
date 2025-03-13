84c19581bb1eda6aacbfdfa42f572cc135bfacb7
IanRock542 <i32505@gmail.com>
2025-03-13 18:46:23 -0500

    get_titles() edits

--- Yt_playlist_vid_titles_exporter-YouTube API-version.py
+++ Yt_playlist_vid_titles_exporter-YouTube API-version.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 api_key = os.environ.get('GOOGLE_API_KEY') # api key hidden in enviroment var
 youtube = build('youtube', 'v3', developerKey=api_key) # sets yt api version
 
-
 now = datetime.now() # time string to differntiate between playlists with the same title
 dt_string = now.strftime("%m-%d-%Y %H-%M-%S")
 
@@ -26,28 +25,26 @@ def get_titles(playlist_id: str, pl_titles: list) -> list:
             maxResults=500,
             playlistId= playlist_id
         )
-
         response = request.execute()
+
+        while request:
+            for video in range(len(response['items'])): # items = videos in playlist
+                pl_titles.append((response['items'][video]['snippet']['title']))
+        
+            nextPageToken = response.get('nextPageToken') # goes to next page of videos
+
+            while nextPageToken: # runs until there are no more pages to loop through
+                response = youtube.playlistItems().list(
+                    part="snippet,contentDetails",
+                    playlistId = playlist_id,
+                    pageToken = nextPageToken
+                ).execute()
+                for video in range(len(response['items'])):
+                    pl_titles.append((response['items'][video]['snippet']['title']))
+                nextPageToken = response.get('nextPageToken')
     except googleapiclient.errors.HttpError or IndexError as e:
         print("You did not enter a valid playlist.")
-        exit(0)
-    else:
-        for video in range(len(response['items'])): # items = videos in playlist
-            pl_titles.append((response['items'][video]['snippet']['title']))
-    
-        nextPageToken = response.get('nextPageToken') # goes to next page of videos
-
-        while nextPageToken: # runs until there are no more pages to loop through
-            response = youtube.playlistItems().list(
-                part="snippet,contentDetails",
-                playlistId = playlist_id,
-                pageToken = nextPageToken
-            ).execute()
-            for video in range(len(response['items'])):
-                pl_titles.append((response['items'][video]['snippet']['title']))
-            nextPageToken = response.get('nextPageToken')
-
-    
+        return None
     return pl_titles
 
 def get_playlist_title(playlist_id: str) -> str:
@@ -63,9 +60,6 @@ def get_playlist_title(playlist_id: str) -> str:
 
 def main() -> None:
     """Runs main code to let user pick playlist and save it"""
-
-    choose_dir()
-
     playlist_input = input("Enter a YouTube Playlist: ")
     playlist = playlist_input[38:] #t rims url, so that only playlist id remains
     print("\nPlaylist id: {}".format(playlist) + "\n")
